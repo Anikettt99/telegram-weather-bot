b731f9c57b0e323e40722c10b1d68dfd77810ec3
Aniket <aniketsinha99123490@gmail.com>
2023-09-08 13:58:52 +0530

    first commit

---
+++ weather-bot-client/src/App.js
@@ -0,0 +1,40 @@
+import axios from "axios";
+import "./App.css";
+import { GoogleOAuthProvider, GoogleLogin } from "@react-oauth/google";
+import { useStore } from "./hooks/useStore";
+import MainPage from "./components/MainPage";
+
+function App() {
+  const setAuthData = useStore((state) => state.setAuthData);
+  const { authData } = useStore();
+  return (
+    <div>
+      {!Object.keys(authData).length && (
+        <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}>
+          <div>
+            <GoogleLogin
+              onSuccess={async (credentialResponse) => {
+                const response = await axios.post(
+                  `${process.env.REACT_APP_SERVER_BASE_URL}/admin/login`,
+                  {
+                    token: credentialResponse.credential,
+                  }
+                );
+                const data = response.data;
+
+                localStorage.setItem("authData", JSON.stringify(data));
+                setAuthData(data);
+              }}
+              onError={() => {
+                console.log("Login Failed");
+              }}
+            />
+          </div>
+        </GoogleOAuthProvider>
+      )}
+      <div>{Object.keys(authData).length ? <MainPage /> : <></>}</div>
+    </div>
+  );
+}
+
+export default App;
---
+++ weather-bot-client/src/components/MainPage.js
@@ -0,0 +1,87 @@
+import { googleLogout } from "@react-oauth/google";
+import { useStore } from "../hooks/useStore";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import "./Style.css";
+
+const MainPage = () => {
+  const { authData, setAuthData } = useStore();
+  const [userList, setUserList] = useState([]);
+  const [reload, setReload] = useState(false);
+
+  useEffect(() => {
+    axios
+      .get(`${process.env.REACT_APP_SERVER_BASE_URL}/telegram/users`)
+      .then((response) => {
+        setUserList(response.data);
+      });
+  }, [reload]);
+
+  const getFormattedDate = (date) => {
+    const utcTimestamp = new Date(date);
+
+    const istTimestamp = new Date(
+      utcTimestamp.getTime() + 5 * 60 * 60 * 1000 + 30 * 60 * 1000
+    );
+
+    const options = { year: "numeric", month: "long", day: "numeric" };
+    const formattedDate = istTimestamp.toLocaleDateString("en-IN", options);
+    return formattedDate;
+  };
+
+  const addSubscription = (userId) => {
+    axios
+      .patch(`${process.env.REACT_APP_SERVER_BASE_URL}/telegram/${userId}`, {
+        status: true,
+      })
+      .then(() => {
+        setReload(!reload);
+      });
+  };
+
+  const removeSubscription = (userId) => {
+    axios
+      .patch(`${process.env.REACT_APP_SERVER_BASE_URL}/telegram/${userId}`, {
+        status: false,
+      })
+      .then(() => {
+        setReload(!reload);
+      });
+  };
+
+  return (
+    <div>
+      <button
+        className="logout-button"
+        onClick={() => {
+          googleLogout();
+          localStorage.setItem("authData", "{}");
+          setAuthData({});
+        }}
+      >
+        Logout
+      </button>
+      <h2>Subscription List</h2>
+      <ul>
+        {userList.map((user) => (
+          <li key={user._id}>
+            <p>{user.userName || user.firstName}</p>{" "}
+            <span className="dot-before"></span>{" "}
+            {`Joined: ${getFormattedDate(user.createdAt)}`}
+            {user.isSubscriptionActive ? (
+              <button onClick={() => removeSubscription(user._id)}>
+                Remove Subscription
+              </button>
+            ) : (
+              <button onClick={() => addSubscription(user._id)}>
+                Add Subscription
+              </button>
+            )}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default MainPage;
